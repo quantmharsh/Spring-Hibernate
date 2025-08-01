36af32c22ec59e4e5b4e8119527e19b1a586e64f
Harsh Srivastava <srivastavah240@gmail.com>
2025-08-01 20:40:17 +0530

    (Feat):added dependencys for hibernate , mysql ,created alien class

---
+++ src/main/java/com/harsh/Main.java
@@ -0,0 +1,13 @@
+package com.harsh;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Alien a1= new Alien();
+        a1.setAid(101);
+        a1.setAname("Rohit");
+        a1.setTech("Java");
+    }
+}
