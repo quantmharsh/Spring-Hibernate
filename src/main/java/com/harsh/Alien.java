c8569e7fb732c166ddf1b0e531178183a1b0bec3
Harsh Srivastava <srivastavah240@gmail.com>
2025-08-02 15:07:44 +0530

    (Feat):CRUD Operations implemented using Hibernate , Postgresql

--- src/main/java/com/harsh/Alien.java
+++ src/main/java/com/harsh/Alien.java
@@ -1,13 +1,20 @@
 package com.harsh;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 //to treat this class as a table
-@Entity
+//to rename or having diffrent name in db we can use name="newname". use same name is hql
+@Entity(name = "alien_database")
+//change table name only
+//@Table(name="alien_db")
 public class Alien {
 //    make aid as a primary key .  important to have primary key
     @Id
+    //rename or have diffrent name of column in database
+    @Column(name = "a_id")
     private int aid;
     private String aname;
     private String tech;
--- src/main/java/com/harsh/Main.java
+++ src/main/java/com/harsh/Main.java
@@ -12,9 +12,9 @@ public class Main {
     public static void main(String[] args) {
 
         Alien a1= new Alien();
-        a1.setAid(101);
-        a1.setAname("Rohit");
-        a1.setTech("Java");
+        a1.setAid(102);
+        a1.setAname("Harsh");
+        a1.setTech("DevOps");
 
         // Step 1: Create a Hibernate Configuration object
         Configuration config = new Configuration();
@@ -33,15 +33,32 @@ public class Main {
 // Step 5: Open a session to interact with the database
         Session session = factory.openSession();
 
-// Step 6: Start a transaction (all DB operations must be within a transaction)
+// Step 6: Start a transaction (all DB operations must be within a transaction).Not required when fetching data from db
         Transaction transaction = session.beginTransaction();
 
-// Step 7: Save an entity object to the database
+// Step 7: Save an entity object to the database or get the data
 // 'a1' must be an instance of Alien (or your entity class)
-        session.persist(a1);
+//        session.persist(a1);
+        // FETCH DATA
+        //We dont required trabsaction while fetching info from db
+        // Eager Fetching: Data is fetched even if we use it or not use it
+//        Alien a1 =session.find(Alien.class , 101);
+        //Its Lazy fetching. means we will only fetch data if we are using it anywhere .otherwise data will not be fetched
+//        Alien a1=session.byId(Alien.class).getReference(101);
+//        System.out.println("A1" +a1);
 
+
+        // UPDATE DATA (TRANSACTION required). it can also be used to create new record
+        session.merge(a1);
+
+        // DELETE DATA
+        Alien a2= session.find(Alien.class , 102);
+        session.remove(a2);
 // Step 8: Commit the transaction to finalize changes
         transaction.commit();
+
+        session.close();
+        factory.close();
 
 
 
