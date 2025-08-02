19584a2334364a78c8a0942069be8f0f1717a311
Harsh Srivastava <srivastavah240@gmail.com>
2025-08-02 14:09:58 +0530

    (Feat):learned to configure hibernate.cfg.xml , save data in db ,create table

--- src/main/java/com/harsh/Alien.java
+++ src/main/java/com/harsh/Alien.java
@@ -1,6 +1,13 @@
 package com.harsh;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+//to treat this class as a table
+@Entity
 public class Alien {
+//    make aid as a primary key .  important to have primary key
+    @Id
     private int aid;
     private String aname;
     private String tech;
--- src/main/java/com/harsh/Main.java
+++ src/main/java/com/harsh/Main.java
@@ -1,5 +1,11 @@
 package com.harsh;
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -9,5 +15,22 @@ public class Main {
         a1.setAid(101);
         a1.setAname("Rohit");
         a1.setTech("Java");
+
+        //create a configuration
+        Configuration config= new Configuration();
+        config.addAnnotatedClass(com.harsh.Alien.class);
+        //load a config
+        config.configure("hibernate.cfg.xml");
+
+        SessionFactory   factory= config.buildSessionFactory();
+        Session session = factory.openSession();
+        //start the transaction to store data  in db
+        Transaction transaction = session.beginTransaction();
+        session.persist(a1);
+        //commit the changes to database
+        transaction.commit();
+
+
+
     }
 }
